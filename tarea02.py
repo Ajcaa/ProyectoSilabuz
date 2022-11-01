c22f062449afc4520b0958aeb6f1e5d96f890835
BryamVC <bryamvega7@gmail.com>
2022-11-01 07:56:16 -0500

    update funciones agregadas

--- tarea02.py
+++ tarea02.py
@@ -1,12 +1,19 @@
 import requests
 
-url = "https://pokeapi.co/api/v2/generation/"
+pokeapi_url = "https://pokeapi.co/api/v2/generation/"
 num_generation = str(input("\nIngrese la generación: "))
-response = requests.get(url+num_generation).json()
-#print(response)
 
 
-for i in response['pokemon_species']:
-    print(i['name'])
-    #print(type(i['name']['url']))
+def api_get_request_generation(num_generation):
+    return requests.get(pokeapi_url+num_generation).json()
 
+def get_generation_from_response(response):
+    for i in response['pokemon_species']:
+        print(f"Nombre: " + i['name'])
+
+def get_info_from_generation(num_generation):
+    response = api_get_request_generation(num_generation)
+    print(f"\nGeneración {num_generation}:")
+    get_generation_from_response(response)
+    
+get_info_from_generation (num_generation)
